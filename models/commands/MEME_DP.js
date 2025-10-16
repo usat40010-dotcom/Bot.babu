a1a93efaefe67322f7ee7bc1eafae06e26688e9e
KRISHNA BOT MASTER <ravikumar630344222@gmail.com>
2025-10-16 10:39:12 +0530

    Update and rename 0_ARIF-DP-MEME_DP.js to MEME_DP.js

--- models/commands/MEME_DP.js
+++ models/commands/MEME_DP.js
@@ -278,6 +278,6 @@ const fs = global.nodemodule["fs-extra"];
 "https://i.imgur.com/4nFOS0w.jpg" ,
 "https://i.imgur.com/tC2Sy8a.jpg" ,
      ];
-     var callback = () => api.sendMessage({body:`┏━━━━━┓\n     ARIF-BABU                    ✧══•❁😛❁•══✧\n┗━━━━━┛\n\n♥️`,attachment: fs.createReadStream(__dirname + "/cache/AOV.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/AOV.jpg"));  
+     var callback = () => api.sendMessage({body:`┏━━━━━┓\n     KABUTRI-BABU                    ✧══•❁😛❁•══✧\n┗━━━━━┛\n\n♥️`,attachment: fs.createReadStream(__dirname + "/cache/AOV.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/AOV.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/AOV.jpg")).on("close",() => callback());
    };
