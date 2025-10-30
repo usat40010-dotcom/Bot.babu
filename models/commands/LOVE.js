b2aa77f18681b78cdb8efa94aa3247082f316f6d
KRISHNA BOT MASTER <ravikumar630344222@gmail.com>
2025-10-30 15:56:08 +0530

    Update LOVE.js

--- models/commands/LOVE.js
+++ models/commands/LOVE.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(200, 200), 70, 160).composite(circleTwo.resize(200, 200), 630, 160);
+    batgiam_img.composite(circleOne.resize(150, 150), 70, 160).composite(circleTwo.resize(150, 150), 550, 160);
     
     let raw = await batgiam_img.getBufferAsync("image/png");
     
