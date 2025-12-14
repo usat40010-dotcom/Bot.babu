cc0d8d36a6a676f8e1004e273e70c1ae49f7e421
kojiyar331 <kojiyar331@gmail.com>
2025-12-15 00:43:38 +0500

    Update GIRL-STATUS.js

--- models/commands/GIRL-STATUS.js
+++ models/commands/GIRL-STATUS.js
@@ -18,7 +18,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["♥️ KABUTRI-BABU♥️"];
+   var hi = ["♥️ TAHA-BABU♥️"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
